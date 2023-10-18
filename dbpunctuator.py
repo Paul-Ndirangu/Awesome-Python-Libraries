e6b52ba99ad7d0d80448aa5554234b36ffac3f94
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-10-18 13:30:48 +0300

    .

---
+++ dbpunctuator.py
@@ -0,0 +1,21 @@
+from dbpunctuator.inference import Inference, InferenceArguments
+from dbpunctuator.utils import DEFAULT_ENGLISH_TAG_PUNCTUATOR_MAP
+
+args = InferenceArguments(
+        model_name_or_path="Qishuai/distilbert_punctuator_en",
+        tokenizer_name="Qishuai/distilbert_punctuator_en",
+        tag2punctuator=DEFAULT_ENGLISH_TAG_PUNCTUATOR_MAP
+    )
+
+punctuator_model = Inference(inference_args=args, 
+                             verbose=False)
+    
+    
+text = [
+    """
+however when I am elected I vow to protect our American workforce
+unlike my opponent I have faith in our perseverance our sense of trust and our democratic principles will you support me
+    """
+]
+
+print(punctuator_model.punctuation(text)[0])
