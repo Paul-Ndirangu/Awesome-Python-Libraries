fa5921e37b5ffe0570e4ee88a5addc5e6ac89b67
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-11-22 10:34:04 +0300

    .

--- datetime/pendulum/standard_datetime.py
+++ datetime/pendulum/standard_datetime.py
@@ -7,6 +7,9 @@ now.in_timezone("America/Toronto")
 
 # Default support for common datetime formats
 now.to_iso8601_string()
+print(f"Time now: {now}")
 
 # Shifting
 now.add(days=2)
+
+print(f" Time after adding 2 days: {now}")
