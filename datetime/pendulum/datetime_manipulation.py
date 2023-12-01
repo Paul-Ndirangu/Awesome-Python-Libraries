6d956038715d419f929f8821c71f5b0786552632
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-12-01 11:19:19 +0300

    datetime manipulation

--- datetime/pendulum/datetime_manipulation.py
+++ datetime/pendulum/datetime_manipulation.py
@@ -3,4 +3,19 @@ import pendulum
 
 # creating datetime instance
 dt = pendulum.datetime(2023, 12, 1)
-print(dt)
+print(f"Initial Datetime Created: {dt}")
+
+# Manipulating datetime object using add()
+dt = dt.add(years=5)
+print(f"Datetime after 5 years: {dt}")
+
+# Manipulating datetime object using subtract()
+dt = dt.subtract(years=4)
+
+
+# Similarly you can add or subtract
+# months,weeks,days,hours,minutes 
+# individually or all at a time.
+dt = dt.add(years=3, months=2, days=6,
+            hours=7, minutes=8, seconds=45)
+print(f"Manipulate Date and Time: {dt}")
