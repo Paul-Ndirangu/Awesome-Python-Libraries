3e30dafda2f9fb33239ece726b46accd77d5fd1a
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-12-01 16:53:33 +0300

    .

--- datetime/pendulum/timedelta_replacement.py
+++ datetime/pendulum/timedelta_replacement.py
@@ -1,6 +1,6 @@
 import pendulum
 
 time_delta = pendulum.duration(years=2, months=7, 
-                               days=2, hours=1, 
+                               weeks=3, days=2, hours=1, 
                                minutes=1, seconds=20)
 print(f"Duration: {time_delta}")
