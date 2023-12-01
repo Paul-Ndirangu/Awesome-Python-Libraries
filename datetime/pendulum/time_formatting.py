82497500db73ce3d7a0fb1655c8f9ab086671fd5
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-12-01 16:42:04 +0300

    .

--- datetime/pendulum/time_formatting.py
+++ datetime/pendulum/time_formatting.py
@@ -3,5 +3,9 @@ import pendulum
 
 # Creating new DateTime Instance
 dt = pendulum.datetime(2020, 11, 27, 12, 30, 15)
-print(f"Created DateTime {dt}")
+print(f"Created DateTime: {dt}")
 
+# Formatting DateTime Instance
+dt.to_day_datetime_string()
+formatted_str = dt.format('dddd Do [of] MMMM YYYY HH:mm:ss A')
+print(f"Formated DateTime: {formatted_str}")
